bb288a9d3b979301c8a756b5ab372d9dc8d1c56c
Emmett McDermott <drewemmett123@gmail.com>
2025-04-23 11:20:55 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -18,11 +18,11 @@ const FirebaseStorage = {
       
       // Initialize Firebase app
       const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_PROJECT.firebaseapp.com",
+        apiKey: "YOUR_API_KEY_HERE",
+        authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
         projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_PROJECT.appspot.com",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+        storageBucket: "YOUR_PROJECT_ID.appspot.com",
+        messagingSenderId: "YOUR_SENDER_ID",
         appId: "YOUR_APP_ID"
       };
       
